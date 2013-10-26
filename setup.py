b845f5ea711d88553e9f3a82eb1c15f1304b0479
Lau Hing Lung <lung220@gmail.com>
2013-10-26 14:35:59 +0800

    Update `setup.py` with classifiers and keywords

--- setup.py
+++ setup.py
@@ -2,13 +2,22 @@ from setuptools import setup, find_packages
 
 setup(
     name="HtmlNode",
-    version="0.1.5",
+    version="0.1.6",
     packages=find_packages(),
     description="A simple Python HTML generator",
     author="Hing-Lung Lau",
     author_email="lung220@gmail.com",
     url="http://github.com/hllau/html_node",
     license="Apache v2",
-    keywords="html builder factory template"
+    keywords="html builder factory template generator",
+    classifiers = [ 
+    'Environment :: Web Environment',
+    'Intended Audience :: Developers',
+    'Programming Language :: Python :: 2.7',
+    'Topic :: Software Development :: Code Generators',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Topic :: Text Processing :: Markup :: HTML',
+    'License :: OSI Approved :: Apache Software License',
+    ]
 )
 
